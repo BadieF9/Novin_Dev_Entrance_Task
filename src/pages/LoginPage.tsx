26760a05cd8fa90d0d6ed932288ee15e91cfdd56
Badie <badie.farzandi@gmail.com>
2025-01-21 14:41:43 +0330

    Some form validation changed in Login and Register pages

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -8,7 +8,6 @@ const schema = yup.object({
   email: yup.string().email().required("Email is required"),
   password: yup
     .string()
-    .min(8, "Password must be at least 8 characters")
     .required("Password is required"),
 });
 
--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -6,7 +6,10 @@ import { Link, useNavigate } from "react-router-dom";
 
 const schema = yup.object({
   email: yup.string().email().required("Email is required"),
-  password: yup.string().required("Password is required"),
+  password: yup
+    .string()
+    .min(8, "Password must be at least 8 characters")
+    .required("Password is required"),
   confirmPassword: yup
     .string()
     .nullable()
